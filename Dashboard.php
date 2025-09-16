710d78962c40e807b25a5c2ccd3c8c710392ee4e
Chathuka Priyankara Edirisinghe <chathukapriyankaraedirisinghe@gmail.com>
2025-09-17 04:41:37 +0530

    dashboard.php10

--- Dashboard.php
+++ Dashboard.php
@@ -152,3 +152,8 @@ if(isset($_GET['action'])){
     </div>
   </div>
 </div>
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
+<script src="assets/js/dashboard.js"></script>
+</body>
+</html>
